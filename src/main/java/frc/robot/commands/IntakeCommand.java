05b46b074d227651e64d4b4b34147376005a7674
Matty Ames <ames1mw@gmail.com>
2024-01-20 09:39:46 -0500

    Reapply "Merge pull request #1 from qbranch4327/4327_2024_Pablo_Code"
    This reverts commit f1f4ca86df9ab15f6cc32fd43b14eeff58072f32.

--- src/main/java/frc/robot/commands/IntakeCommand.java
+++ src/main/java/frc/robot/commands/IntakeCommand.java
@@ -8,17 +8,14 @@ import frc.robot.subsystems.*;
 public class IntakeCommand extends Command {
     
     IntakeSubsystem intakeSubsystem;
-    SpinnerSubsystem spinnerSubsystem;
     Joystick controller1;
     XboxController controller2;
 
-    public IntakeCommand(IntakeSubsystem intakeSubsystem, SpinnerSubsystem spinnerSubsystem, XboxController controller2, Joystick controller1)  {
+    public IntakeCommand(IntakeSubsystem intakeSubsystem, XboxController controller2, Joystick controller1)  {
         this.intakeSubsystem = intakeSubsystem;
-        this.spinnerSubsystem = spinnerSubsystem;
         this.controller1 = controller1;
         this.controller2 = controller2;
         addRequirements(intakeSubsystem);
-        addRequirements(spinnerSubsystem);
     }
 
     @Override
@@ -37,22 +34,6 @@ public class IntakeCommand extends Command {
         }
         else    {
           intakeSubsystem.intakeOff();
-        }
-        // if (controller2.getLeftX()>.2)   {
-        //     spinnerSubsystem.spin(true);
-        // }
-        // else if (controller2.getLeftX()<-.2){
-        //     spinnerSubsystem.spin(false);
-        // }
-        // else {
-        //     spinnerSubsystem.stop();
-        // }        
-
-        if (controller2.getLeftBumper())    {
-            intakeSubsystem.grab();
-        }
-        else    {
-            intakeSubsystem.release();
         }
     }
 
---
+++ src/main/java/frc/robot/subsystems/ShooterSubsytem.java
@@ -0,0 +1,39 @@
+package frc.robot.subsystems;
+import com.revrobotics.CANSparkFlex;
+import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+
+import edu.wpi.first.wpilibj.Servo;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class ShooterSubsytem extends SubsystemBase {
+    
+    CANSparkFlex shooterMotor1;
+    CANSparkFlex shooterMotor2;
+    Servo shooterServo;
+
+
+    public ShooterSubsytem()    {
+        shooterMotor1 = new CANSparkFlex(0,  null);
+        shooterMotor2 = new CANSparkFlex(1, null);
+        shooterServo = new Servo(0);
+    }
+
+
+    public void shooterOn() {
+        shooterMotor1.set(1);
+        shooterMotor2.set(1); 
+    }
+
+    public void stop()    {
+        shooterMotor1.stopMotor();
+        shooterMotor2.stopMotor();
+    }
+
+    public void shoot()   {
+        shooterServo.set(1);
+    }
+
+    public void resetServo()  {
+        shooterServo.set(0);
+    }
+}
