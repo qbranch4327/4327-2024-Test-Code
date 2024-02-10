6088a2e1f9ff092d28466388fcab1dc8e6f19987
Elise Segal <elisesegal@gmail.com>
2024-02-10 09:40:01 -0500

    One more

--- src/main/java/frc/robot/commands/AutonCommands/AutonArmDownCommand.java
+++ src/main/java/frc/robot/commands/AutonCommands/AutonArmDownCommand.java
@@ -1,49 +1,27 @@
-package frc.robot.commands;
+package frc.robot.commands.AutonCommands;
 
-import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj2.command.Command;
-import frc.robot.subsystems.IntakeSubsystem;
 import frc.robot.subsystems.RotationArmSubsystem;
-import frc.robot.subsystems.ShooterSubsystem;
-import frc.robot.subsystems.Swerve;
 import frc.robot.subsystems.WristSubsystem;
 
-public class AutonArmCommand extends Command    {
+public class AutonArmDownCommand extends Command    {
     
     RotationArmSubsystem rotationArmSubsystem;
     WristSubsystem wristSubsystem;
-    IntakeSubsystem intakeSubsystem;
-    ShooterSubsystem shooterSubsystem;
-    Timer timer;
-    Swerve swerve;
     boolean isItFinished;
 
-    public AutonArmCommand(RotationArmSubsystem rotationArmSubsystem, Swerve swerve, WristSubsystem wristSubsystem, IntakeSubsystem intakeSubsystem, ShooterSubsystem shooterSubsystem) {
+    public AutonArmDownCommand(RotationArmSubsystem rotationArmSubsystem, WristSubsystem wristSubsystem) {
         this.rotationArmSubsystem = rotationArmSubsystem;
-        this.swerve = swerve;
         this.wristSubsystem = wristSubsystem;
-        this.intakeSubsystem = intakeSubsystem;
-        this.shooterSubsystem = shooterSubsystem;
         isItFinished = false;
-        timer = new Timer();
+        addRequirements(rotationArmSubsystem);
+        addRequirements(wristSubsystem);
     }
 
     @Override
     public void execute()   {
         if (rotationArmSubsystem.wentTo(0.291) && wristSubsystem.wentTo(0.367, 0.1)) {
-            swerve.goForward();
-            intakeSubsystem.intakeOn(true);
-            if (!intakeSubsystem.sensor.get())   {
-                swerve.stop();
-                intakeSubsystem.intakeOff();
-                if (rotationArmSubsystem.wentTo(0.043) && wristSubsystem.wentTo(0.367, 0.1))    {
-                    intakeSubsystem.intakeOn(true);
-                    if (!shooterSubsystem.sensor.get())   {
-                        intakeSubsystem.intakeOff();
-                        isItFinished = true;
-                    }
-                }
-            }
+            isItFinished = true;
         }
     }
 
